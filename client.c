fd13d678edc8c4b67288353389c33f37082bc634
kristerssaulitis <kristers.saulitis1@gmail.com>
2021-11-07 11:25:21 +0000

    ienput check, and first connection

--- client.c
+++ client.c
@@ -5,59 +5,19 @@
 #include <unistd.h>
 #include <netdb.h>
 #include "hw1.h"
+#include <stdlib.h>
+#include <string.h>
 
 char* host;
-char* port;
+int port;
+struct sockaddr_in remote_address;
+int my_socket;
+char* servername;
 
-void sender(){
+void *connection_handler(void *socket_desc);
 
-}
-
-char* inputCheck(char*  haystack, char* needle ){
-    char* result;
-    int i = 0;
-    char* ret = strstr(haystack, needle);
-    /*printf("this is ret %s", ret);*/
-    if (strcmp(ret, "-a=") >0 || strcmp(ret, "-p=") >0){
-        for (i = 2; i< strlen(haystack); i++){
-            result[i-2] = haystack[i];
-        }
-        return result;
-    }
-}
-
-int main(int argc, char ** argv){
-
-    if (argc < 3){
-        printf("not enough arguments \n");
-        return -1;
-    }
-    if (strcmp("client", argv[1]) < 0){
-        printf("wrong input, client not called\n");
-    }
-    host = inputCheck(argv[2], "-a=");
-    port = inputCheck(argv[3], "-p=");
-
-    printf("this is host :%s and this is port:%s", host, port);
-
-
-    int my_socket = 0;
-    char* servername;
-    struct sockaddr_in remote_address;
-    char inputs[100];
-    remote_address.sin_family = AF_INET;
-    remote_address.sin_port = htons(PORT);
-    servername= gethostbyname(HOST);
-    inet_pton(AF_INET, servername, &remote_address.sin_addr);
-
-    if((my_socket = socket(AF_INET,SOCK_STREAM,0)) < 0){
-        printf("SOCKET ERROR\n");
-        return -1;
-    }
-    /* threading*/
-
-    /*
-     if(connect(my_socket,(struct  sockaddr *) &remote_address,sizeof(remote_address)) < 0)
+void *connection_handler(void *socket_desc){
+    if(connect(my_socket,(struct  sockaddr *) &remote_address,sizeof(remote_address)) < 0)
     {
         printf("ERROR connecting\n");
         return -1;
@@ -68,9 +28,61 @@ int main(int argc, char ** argv){
             scanf("%s", inputs);
             //strcat("\n", inputs);
             send(my_socket, inputs, strlen(inputs), 0);
+            /*read also some stuff - ja var nolasīt, tad var izprintet*/
         }
     }
-    */
+
+}
+
+int main(int argc, char ** argv){
+
+    if (argc < 2){
+        printf("not enough arguments \n");
+        return -1;
+    }
+
+    int i = 0;
+    if (strncmp("-a=", argv[1],3) < 0){
+        printf("wrong parameter HOST");
+        return -1;
+    }
+    if (strncmp("-p=", argv[2],3) < 0){
+        printf("wrong parameter PORT %s \n", argv[2]);
+        return -1;
+    }
+
+    char* realport;
+    char *randomhost = argv[1];
+    char *ranodmport = argv[2];
+    char* found;
+    for (i =0; i< 2; i++) host = strsep(&randomhost,"=");
+    for (i =0; i< 2; i++) realport = strsep(&ranodmport,"=");
+    port = atoi(realport);
+
+    printf("this is host :%s and this is port:%i      ", host, port);
+
+
+    my_socket = 0;
+
+
+    char inputs[100];
+    remote_address.sin_family = AF_INET;
+    remote_address.sin_port = htons(port);
+    servername= gethostbyname(host);
+    inet_pton(AF_INET, servername, &remote_address.sin_addr);
+
+    if((my_socket = socket(AF_INET,SOCK_STREAM,0)) < 0){
+        printf("SOCKET ERROR\n");
+        return -1;
+    }
+    /* threading*/
+
+    int *socketname = malloc(sizeof(int));
+    *socketname = my_socket;
+
+    pthread_t tred;
+    pthread_create(&tred, NULL, connection_handler, socketname);
+    pthread_join(tred, 0);
 
     return 0;
 }
